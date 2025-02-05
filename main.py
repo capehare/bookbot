5056376da216269ad8aff46a674a87227a6d09bf
capehare <cape@haremail.slmail.me>
2025-02-05 20:16:37 +0100

    added character counter and character sorter functions

--- main.py
+++ main.py
@@ -10,15 +10,49 @@ def word_count(text):
         word_counter += 1
     return word_counter
 
+def chara_counter(text):
+    """Defining a function that reads individual characters in a given
+    text, counts them and adds them to a dictionary as individual keys 
+    with their respective counts. Returns a dictionary.
+    """
+    characters = text.lower()
+    chara_dict = {}
+    for character in characters:
+        if character in chara_dict:
+            chara_dict[character] += 1
+        else:
+            chara_dict[character] = 1
+    return chara_dict
+
+def chara_sort(chara_count):
+    """Defining a function that takes a dictionary specifically structured as 
+    alphanumeric:integer entries, removes all keys that are not alphanumeric, saves 
+    them as a list of tuples and sorts the list from greatest value to lowest. Returns 
+    a list of tuples as alphanumeric,integer entries
+    """
+    clean_list = []
+    for chara in chara_count:
+        value = chara_count[chara]
+        if chara.isalpha():
+            clean_list.append((chara, value))
+    def sort_on(item):
+        return item[1]
+    clean_list.sort(reverse=True, key=sort_on)
+    return clean_list
 
 def main():
     """Main function that opens a text using a with block and feeds
-    it into the word_count function.
+    it into the word_count and chara_count functions. The chara_count
+    result is fed into the chara_sort function.
     Prints the result.
     """
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         word_count_result = word_count(file_contents)
+        chara_count_result = chara_counter(file_contents)
+        clean_list_result = chara_sort(chara_count_result)
         print(word_count_result)
+        print(chara_count_result)
+        print(clean_list_result)
 
 main()
